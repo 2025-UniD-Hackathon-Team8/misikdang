0272fc8af5ff472019c4dcbe8ae593b5e477636f
Humanthinking <cho578956@gmail.com>
2025-11-16 07:07:32 +0900

    main update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import OwnerRegisteredMenuPage from './pages/OwnerRegisteredMenuPage.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import OwnerRegisteredMenuPage from "./pages/OwnerRegisteredMenuPage.tsx";
+import OnboardingPage from "./pages/OnboardingPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <OwnerRegisteredMenuPage />
-  </StrictMode>,
-)
+    <OnboardingPage />
+  </StrictMode>
+);
