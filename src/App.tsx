f5cb47b34db731afbe30707710c6a40214245e3c
Humanthinking <cho578956@gmail.com>
2025-11-16 02:35:52 +0900

    WIP: Temporary changes to App.tsx before merge

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 // import Button from "./components/Button"; // Button 컴포넌트는 사용하지 않으므로 주석 처리하거나 제거 가능
 
 // 1. components 폴더에서 Profile 컴포넌트를 import 합니다.
-import Profile from "./components/Profile";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
